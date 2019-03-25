2dd672ef67663fb8117801d3641c44bc737692d5
8084 <klntsky@gmail.com>
2019-03-25 22:26:36 +0300

    added: settings reset, domains to settings page

--- src/Settings.purs
+++ src/Settings.purs
@@ -2,14 +2,22 @@ module Settings where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
+import Data.Array (mapWithIndex)
+import Data.Array as A
+import Data.Lens (set, (%~))
+import Data.Lens.Index (ix)
+import Data.Lens.Record (prop)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
 import Halogen as H
-import Halogen.HTML as HH
+import Halogen.HTML (HTML, br_, div_, h3_, input, label, text)
+import Halogen.HTML.Events (onChecked, onClick)
 import Halogen.HTML.Events as HE
-import Halogen.HTML.Properties as HP
+import Halogen.HTML.Properties
 import SettingsFFI (save)
-
+import Web.HTML (window)
+import Web.HTML.Window (confirm)
 
 type State =
   { includeMuted :: Boolean
@@ -18,6 +26,10 @@ type State =
   , sortBackwards :: Boolean
   , menuOnTab :: Boolean
   , menuOnButton :: Boolean
+  , markAsAudible :: Array { domain :: String
+                           , enabled :: Boolean
+                           , withSubdomains :: Boolean
+                           }
   }
 
 
@@ -28,10 +40,23 @@ data CheckBox
   | SortBackwards
   | MenuOnTab
   | MenuOnButton
+  | FilterEnabled Int
+  | FilterWithSubdomains Int
+
+
+data Button
+  = RemoveDomain Int
+  | RestoreDefaults
+
+
+data Input
+  = DomainField Int String
 
 
 data Query a
   = Toggle CheckBox Boolean a
+  | Click Button a
+  | TextInput Input a
 
 
 type Message = Unit
@@ -45,10 +70,24 @@ initialState =
     , sortBackwards: false
     , menuOnTab: true
     , menuOnButton: true
+    , markAsAudible:
+      [ { domain: "soundcloud.com"
+        , enabled: false
+        , withSubdomains: false
+        }
+      , { domain: "bandcamp.com"
+        , enabled: false
+        , withSubdomains: true
+        }
+      , { domain: "youtube.com"
+        , enabled: false
+        , withSubdomains: false
+        }
+      ]
     }
 
 
-mkComponent :: State -> H.Component HH.HTML Query Unit Message Aff
+mkComponent :: State -> H.Component HTML Query Unit Message Aff
 mkComponent state = H.component
     { initialState: const state
     , render
@@ -56,86 +95,142 @@ mkComponent state = H.component
     , receiver: const Nothing
     }
   where
-
   render :: State -> H.ComponentHTML Query
   render { includeMuted, allWindows, includeFirst, sortBackwards
-         , menuOnTab, menuOnButton } =
-    HH.div_
-    [ HH.h3_ [ HH.text "GENERAL SETTINGS" ]
+         , menuOnTab, menuOnButton, markAsAudible } =
+    div_
+    [ h3_ [ text "GENERAL SETTINGS" ]
 
-    , HH.div_
-      [ HH.input [ HP.type_ HP.InputCheckbox
-                 , HP.checked includeMuted
-                 , HE.onChecked (HE.input (Toggle IncludeMuted))
-                 , HP.id_ "includeMuted"
+    , div_
+      [ input [ type_ InputCheckbox
+                 , checked includeMuted
+                 , onChecked (HE.input (Toggle IncludeMuted))
+                 , id_ "includeMuted"
                  ]
-      , HH.label
-        [ HP.for "includeMuted" ]
-        [ HH.text "Include muted tabs" ]
+      , label
+        [ for "includeMuted" ]
+        [ text "Include muted tabs" ]
       ]
 
-    , HH.div_
-      [ HH.input [ HP.type_ HP.InputCheckbox
-                 , HP.checked allWindows
-                 , HE.onChecked (HE.input (Toggle AllWindows))
-                 , HP.id_ "allWindows"
+    , div_
+      [ input [ type_ InputCheckbox
+                 , checked allWindows
+                 , onChecked (HE.input (Toggle AllWindows))
+                 , id_ "allWindows"
                  ]
-      , HH.label
-        [ HP.for "allWindows" ]
-        [ HH.text "Search for audible tabs in all windows" ]
+      , label
+        [ for "allWindows" ]
+        [ text "Search for audible tabs in all windows" ]
       ]
 
-    , HH.div_
-      [ HH.input [ HP.type_ HP.InputCheckbox
-                 , HP.checked sortBackwards
-                 , HE.onChecked (HE.input (Toggle SortBackwards))
-                 , HP.id_ "sortBackwards"
-                 ]
-      , HH.label
-        [ HP.for "sortBackwards" ]
-        [ HH.text "When cycling through tabs, visit them in reverse order (i.e. right-to-left)" ]
+    , div_
+      [ input [ type_ InputCheckbox
+              , checked sortBackwards
+              , onChecked (HE.input (Toggle SortBackwards))
+              , id_ "sortBackwards"
+              ]
+      , label
+        [ for "sortBackwards" ]
+        [ text "When cycling through tabs, visit them in reverse order (i.e. right-to-left)" ]
       ]
 
-    , HH.div_
-      [ HH.input [ HP.type_ HP.InputCheckbox
-                 , HP.checked includeFirst
-                 , HE.onChecked (HE.input (Toggle IncludeFirst))
-                 , HP.id_ "includeFirst"
-                 ]
-      , HH.label
-        [ HP.for "includeFirst" ]
-        [ HH.text "When cycling through tabs, also include first tab from which the cycle was started" ]
+    , div_
+      [ input [ type_ InputCheckbox
+              , checked includeFirst
+              , onChecked (HE.input (Toggle IncludeFirst))
+              , id_ "includeFirst"
+              ]
+      , label
+        [ for "includeFirst" ]
+        [ text "When cycling through tabs, also include first tab from which the cycle was started" ]
       ]
 
-    , HH.h3_ [ HH.text "CONTEXT MENUS" ]
-    , HH.text "Context menus allow to manually mark tabs as audible."
-    , HH.br_
-    , HH.br_
+    , h3_ [ text "CONTEXT MENUS" ]
+    , text "Context menus allow to manually mark tabs as audible."
+    , br_
+    , br_
 
-    , HH.div_
-      [ HH.input [ HP.type_ HP.InputCheckbox
-                 , HP.checked menuOnTab
-                 , HE.onChecked (HE.input (Toggle MenuOnTab))
-                 , HP.id_ "menuOnTab"
-                 ]
-      , HH.label
-        [ HP.for "menuOnTab" ]
-        [ HH.text "Enable for tabs" ]
+    , div_
+      [ input [ type_ InputCheckbox
+              , checked menuOnTab
+              , onChecked (HE.input (Toggle MenuOnTab))
+              , id_ "menuOnTab"
+              ]
+      , label
+        [ for "menuOnTab" ]
+        [ text "Enable for tabs" ]
       ]
 
-    , HH.div_
-      [ HH.input [ HP.type_ HP.InputCheckbox
-                 , HP.checked menuOnButton
-                 , HE.onChecked (HE.input (Toggle MenuOnButton))
-                 , HP.id_ "menuOnButton"
-                 ]
-      , HH.label
-        [ HP.for "menuOnButton" ]
-        [ HH.text "Enable for toolbar button" ]
+    , div_
+      [ input [ type_ InputCheckbox
+              , checked menuOnButton
+              , onChecked (HE.input (Toggle MenuOnButton))
+              , id_ "menuOnButton"
+              ]
+      , label
+        [ for "menuOnButton" ]
+        [ text "Enable for toolbar button" ]
       ]
+
+    , h3_ [ text "MARK DOMAINS" ]
+    , text "Enter below domains which you want to mark as audible permanently. This may be useful when browsing these sites a lot."
+    , br_
+    , br_
+
+    , div_ $
+      markAsAudible `flip mapWithIndex`
+      \ix { domain, enabled, withSubdomains } ->
+      let id = "withSubdomains" <> show ix in
+      div_
+      [ input [ type_ InputCheckbox
+              , onChecked $ HE.input $ Toggle $ FilterEnabled ix
+              , checked enabled ]
+      , input [ value domain
+              , HE.onValueInput $ HE.input (TextInput <<< DomainField ix)
+              ]
+      , input [ type_ InputCheckbox
+              , onChecked $ HE.input $ Toggle $ FilterWithSubdomains ix
+              , id_ id
+              , checked withSubdomains
+              ]
+      , label
+        [ for id ]
+        [ text "Include subdomains" ]
+      , input [ type_ InputButton
+              , onClick  (HE.input $ const (Click (RemoveDomain ix)))
+              , value "Remove"
+              ]
+      ]
+    , input [ type_ InputButton
+            , onClick (HE.input $ const (Click RestoreDefaults))
+            , value "Restore defaults"
+            ]
     ]
 
   eval :: Query ~> H.ComponentDSL State Query Message Aff
+  eval (Click button next) = do
+    case button of
+      RemoveDomain index ->
+        H.modify_ $
+        prop markAsAudible_ %~
+        (\arr -> fromMaybe arr $ A.deleteAt index arr)
+      RestoreDefaults -> do
+        confirmed <- H.liftEffect $
+          window >>= confirm "Do you really want to reset the settings?"
+        when confirmed do
+          H.put initialState
+
+    pure next
+
+  eval (TextInput input next) = do
+    case input of
+      DomainField index str ->
+        H.modify_ $
+        prop markAsAudible_ %~
+        ix index %~
+        set (prop domain_) str
+
+    pure next
   eval (Toggle checkbox value next) = do
     H.modify_ case checkbox of
       IncludeMuted  -> (_ { includeMuted  = value })
@@ -144,6 +239,20 @@ mkComponent state = H.component
       SortBackwards -> (_ { sortBackwards = value })
       MenuOnTab     -> (_ { menuOnTab     = value })
       MenuOnButton  -> (_ { menuOnButton  = value })
+      FilterEnabled index ->
+        prop markAsAudible_ %~
+        ix index %~
+        set (prop enabled_) value
+      FilterWithSubdomains index ->
+        prop markAsAudible_ %~
+        ix index %~
+        set (prop withSubdomains_) value
+
     values <- H.get
     H.liftAff (save values)
     pure next
+
+  withSubdomains_ = SProxy :: SProxy "withSubdomains"
+  domain_ = SProxy :: SProxy "domain"
+  enabled_ = SProxy :: SProxy "enabled"
+  markAsAudible_ = SProxy :: SProxy "markAsAudible"
